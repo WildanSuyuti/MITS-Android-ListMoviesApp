905b7b581d0f492e4743aa3c675f559f82dd2feb
Muhammad Wildan Suyuti <em.wildan.es@gmail.com>
2016-12-26 12:34:54 +0700

    rename activity_main to activity_horizontal

--- app/src/main/java/com/mits/kakaroto/listmovieapp/HorizontalActivity.java
+++ app/src/main/java/com/mits/kakaroto/listmovieapp/HorizontalActivity.java
@@ -29,7 +29,7 @@ public class HorizontalActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_horizontal);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         initRecyclerView();
 
